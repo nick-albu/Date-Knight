ae099d33d866470842c58e2ac01503414bd87041
Nick Albu <nicolasmalbu@gmail.com>
2022-03-03 14:46:28 -0500

    Choice functionality working in Food.js

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -1,23 +1,33 @@
 import './Food.scss'
-import React, { useEffect, useState } from "react";
+import foodArr from '../../data/food.json'
+import React, { useState } from "react";
 
 function Food() {
-    const [visible, setVisibility] = useState(false);
 
-    // I need to pull data from json files and map through the food options to create links for
-    // the dropdown list.
-    
-
+    const [dropdown, setDropdown] = useState(false);
+    const [choice, setChoice] = useState({});
     
     return (
         <div className='food'>
             <div className="food___dropdownContainer">
-                <button onClick={() => setVisibility(!visible)} class="food__dropdownButton">Choose our food</button>
-                {visible && (
+                <button onClick={() => {setDropdown(!dropdown); setChoice({})}} className="food__dropdownButton">Choose our food</button>
+                {dropdown && (
                     <div className="food__dropdownContent">
-                        <a className='food__drowdownItem' href="#home">Option 1</a>
-                        <a className='food__drowdownItem' href="#about">Option 2</a>
-                        <a className='food__drowdownItem' href="#contact">Option 3</a>
+                        {foodArr.map((food)=>(
+                            <button onClick={() => {
+                                setDropdown(!dropdown); 
+                                setChoice(foodArr.find(function(option, index) {
+                                    if (option.name === food.name)
+                                        return food.name
+                                }))
+                            }} className='food__drowdownItem' href="">{food.name}</button>
+                        ))}
+                    </div>
+                )}
+                {choice && (
+                    <div>
+                        <p>{choice.name}</p>
+                        <p>{choice.details}</p>
                     </div>
                 )}
             </div>
