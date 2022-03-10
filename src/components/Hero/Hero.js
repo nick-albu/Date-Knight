2746b745a46da5966aebe9ff898017575fa6645a
Nick Albu <nicolasmalbu@gmail.com>
2022-03-10 15:06:32 -0500

    setup _mixins.scss

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,14 +6,16 @@ import HeroText from '../../assets/images/herotext.PNG'
 function Hero() {
     return (
         <div className='hero'>
-            <img className='hero__header' src={HeroText} alt='Date Knight text'/>
             <div className='hero__container'>
-                <div className='hero__descContainer'>
-                    <p className='hero__description'>A custom date builder and date generator, together</p>
-                    <p className='hero__description'>Choose your food, activity and dress code</p>
-                    <p className='hero__description'>Go have fun together</p>
+                <img className='hero__header' src={HeroText} alt='Date Knight text'/>
+                <div className='hero__btmContainer'>
+                    <div className='hero__descContainer'>
+                        <p className='hero__description'>A custom date builder and date generator, together</p>
+                        <p className='hero__description'>Choose your food, activity and dress code</p>
+                        <p className='hero__description'>Go have fun together</p>
+                    </div>
+                    <img className='hero__image' src={Knight} alt='date knight hero img'/>
                 </div>
-                <img className='hero__image' src={Knight} alt='date knight hero img'/>
             </div>
         </div>
     )
