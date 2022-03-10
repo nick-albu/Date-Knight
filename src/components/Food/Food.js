7fda62c593f108774fc00005adb8fc631d4b79e2
Nick Albu <nicolasmalbu@gmail.com>
2022-03-10 10:26:29 -0500

    Updated assets package

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -10,12 +10,12 @@ function Food() {
     return (
         <div className='food'>
             <div className="food___dropdownContainer">
-                <div>
-                <button onClick={() => {setFoodDropdown(!foodDropdown); setFood({})}} className="food__dropdownButton">Choose our food</button>
-                <button onClick={() => {
-                    setFoodDropdown(false);
-                    setFood(foodArr[Math.floor(Math.random() * foodArr.length)])
-                    }} className="food__randomButton">Jesus take the wheel</button>
+                <div className="food___buttonContainer">
+                    <button onClick={() => {setFoodDropdown(!foodDropdown); setFood({})}} className="food__dropdownButton">Choose our food</button>
+                    <button onClick={() => {
+                        setFoodDropdown(false);
+                        setFood(foodArr[Math.floor(Math.random() * foodArr.length)])
+                        }} className="food__randomButton">Jesus take the wheel</button>
                 </div>
                 {foodDropdown && (
                     <div className="food__dropdownContent">
